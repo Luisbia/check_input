15d9ba8647461efaefe0e41ae9e92baa16116305
luisbia <luisbia@gmail.com>
2022-11-23 23:48:15 +0100

    confirmation

--- NQR/NQR_script.R
+++ NQR/NQR_script.R
@@ -3,12 +3,7 @@ library(regacc)
 check_packages()
 
 # Only run if an update is needed
-# t1001<- get_denodo("hv_fame_regacc4regacc_t1001")
-# t1002<- get_denodo("hv_fame_regacc4regacc_t1002")
-# t1200<- get_denodo("hv_fame_regacc4regacc_t1200")
-# t1300<- get_denodo("hv_fame_regacc4regacc_t1300")
-# 
-# tables <- bind_rows(t1001, t1002, t1200, t1300)
+tables<- get_denodo("hv_fame_regacc4regacc_all_dbs")
 
 # write_parquet(tables,"data/denodo/all_primary.parquet")
 
---
+++ confirmation/script_confirmation.R
@@ -0,0 +1,55 @@
+
+df<- read_parquet("data/denodo/all_primary.parquet") %>% 
+  filter(country == country_sel)
+
+
+b1g_emp<- df %>% 
+  filter(type =="V" & 
+         table_identifier=="T1001_1200" & 
+         sto %in% c("B1G", "EMP") & 
+         unit_measure %in% c("XDC","PS") & 
+         prices !="Y" & NUTS !=3) 
+
+pop<- df %>% 
+  filter(type =="V" & 
+           table_identifier=="T1001_1200" & 
+           sto %in% c("POP") & 
+           unit_measure %in% c("PS_B") & NUTS !=3) 
+
+
+b1gq<- df %>% 
+  filter(type =="V" &  
+           table_identifier=="T1001_1200" & 
+           sto %in% c("B1GQ") &
+           NUTS!=3 &
+           unit_measure=="PC" &
+           transformation =="G1") 
+
+data<- bind_rows(b1g_emp,pop,b1gq) %>% 
+  left_join(.,dataregacc::NUTS_2021) %>% 
+  select(country,ref_area,label,NUTS,sto,time_period,obs_value,obs_status) %>% 
+  mutate(obs_value=janitor::round_half_up(obs_value,2)) %>% 
+  mutate(obs_status=str_remove_all(obs_status,"00"),
+         obs_status=str_replace_all(obs_status,"F0","P"),
+         obs_status=str_replace_all(obs_status,"G0","E"),
+         obs_status=str_replace_all(obs_status,"Q0","B"),
+         obs_status=str_replace_all(obs_status,"J0","D"),
+         obs_status=str_replace_all(obs_status,"D0","U")) %>%
+  unite("obs_value",c(obs_value,obs_status),sep=" ") %>% 
+  pivot_wider(names_from = time_period,
+              values_from = obs_value)%>% 
+  arrange(sto,ref_area)
+
+final_flags <- df %>% 
+  select(-values) %>% 
+  pivot_wider(names_from = time,
+              values_from = flags)%>% 
+  arrange(na_item,geo)
+
+#write file
+
+wb <- createWorkbook()  
+addWorksheet(wb,paste0(country_sel))
+writeDataTable(wb,paste0(country_sel),data,tableStyle = "TableStyleMedium13")
+
+saveWorkbook(wb, paste0("confirmation/",country_sel,".xlsx"), overwrite = TRUE)
